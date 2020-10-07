67b9a7e3143a1c650e1cf494e89642604b852cfa
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-07 16:39:24 -0400

    Fixed port to comply with heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const port = 3001;
+const port = process.env.PORT || 3001;
 const knex = require('knex');
 
 app.use(express.json());
