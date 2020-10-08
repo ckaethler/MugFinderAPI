0f5ef0b1204b4c41c1138775a3ad075670d3f66f
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-07 22:49:20 -0400

    Changed db origin to heroku postrgres

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ const { handleImage, handleAPICall } = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '',
-      database : 'mugfinder-db'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
